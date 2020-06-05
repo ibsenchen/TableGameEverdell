f3dcc56058d9abbc4d15c5225d9918f38dcb3b5d
ibsenchen <40309901+ibsenchen@users.noreply.github.com>
2020-06-06 00:06:18 +0800

    完善玩家类

--- player.java
+++ player.java
@@ -1,12 +1,21 @@
+package everdell;
+
 import java.util.ArrayList;
 
 public class player {
+    private int chushishoupaishu;
+    private int shoupaishangxian;
     private ArrayList<String> shoupaiqu;
     private ArrayList<String> jianzhuqu;
-
+    private boolean finishgame;
+    private int jijie;//1-春，2-夏，3-秋，4-冬
     public player() {
+        this.chushishoupaishu=5;
+        this.shoupaishangxian=8;
         this.shoupaiqu=new ArrayList<String>();
         this.jianzhuqu=new ArrayList<String>();
+        this.finishgame=false;
+        this.jijie=1;
     }
 
     public ArrayList<String> getShoupaiqu() {
@@ -23,5 +32,41 @@ public class player {
 
     public void addJianzhuqu(String jianzhuqu) {
         this.jianzhuqu.add(jianzhuqu);
+    }
+
+    public int getShoupaishangxian() {
+        return shoupaishangxian;
+    }
+
+    public void setShoupaishangxian(int shoupaishangxian) {
+        this.shoupaishangxian = shoupaishangxian;
+    }
+
+    public int getChushishoupaishu() {
+        return chushishoupaishu;
+    }
+
+    public void setChushishoupaishu(int chushishoupaishu) {
+        this.chushishoupaishu = chushishoupaishu;
+    }
+
+    public boolean isFinishgame() {
+        return finishgame;
+    }
+
+    public void setFinishgame(boolean finishgame) {
+        this.finishgame = finishgame;
+    }
+
+    public int getJijie() {
+        return jijie;
+    }
+
+    public void setJijie(int jijie) {
+        this.jijie = jijie;
+    }
+
+    public void addJijie() {
+        this.jijie++;
     }
 }
