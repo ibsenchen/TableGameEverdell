9f33b0eca6d5a2ec16658b92631cf97157a16767
ibsenchen <40309901+ibsenchen@users.noreply.github.com>
2020-06-06 00:03:32 +0800

    创建动物牌和建筑牌的父类

---
+++ kapai.java
@@ -0,0 +1,69 @@
+package everdell;
+
+public class kapai {
+    private String paimian;
+    private int shuliang;
+    private String leixing;
+    private boolean weiyi;
+    private int fenshu;
+    private String xiaoguoyulan;
+    public void xiaoguo(){
+        System.out.println("效果");
+    }
+
+    public kapai() {
+
+    }
+
+    public String getPaimian() {
+        return paimian;
+    }
+
+    public void setPaimian(String paimian) {
+        this.paimian = paimian;
+    }
+
+    public int getShuliang() {
+        return shuliang;
+    }
+
+    public void setShuliang(int shuliang) {
+        this.shuliang = shuliang;
+    }
+
+    public String getLeixing() {
+        return leixing;
+    }
+
+    public void setLeixing(String leixing) {
+        this.leixing = leixing;
+    }
+
+    public boolean isWeiyi() {
+        return weiyi;
+    }
+
+    public void setWeiyi(boolean weiyi) {
+        this.weiyi = weiyi;
+    }
+
+    public int getFenshu() {
+        return fenshu;
+    }
+
+    public void setFenshu(int fenshu) {
+        this.fenshu = fenshu;
+    }
+
+    public String getXiaoguoyulan() {
+        return xiaoguoyulan;
+    }
+
+    public void setXiaoguoyulan(String xiaoguoyulan) {
+        this.xiaoguoyulan = xiaoguoyulan;
+    }
+
+    public void linkKapai(kapai pai) {
+        return;
+    }
+}
